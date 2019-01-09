870a2761240f475fd12571b3dfff5852accef0bc
vagrant <vagrant@homestead>
2019-01-09 09:43:09 +0000

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+	protected $fillable = ['content'];
+	
     public function user(){
     	//指定一条微博属于一个用户
     	return $this->belongsTo(User::class);
